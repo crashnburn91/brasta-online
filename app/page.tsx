883a2280c18f2ea7d5b038d21084c9c6d7cf8c70
crashnburn91 <105611209+crashnburn91@users.noreply.github.com>
2026-08-24 18:31:44 -0500

    Move Brasta account into global navigation

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,16 @@ import AccountBridge from './AccountBridge';
 import BrastaBootstrap from './BrastaBootstrap';
 
 export default function Home() {
-  return <><BrastaBootstrap /><AccountBridge /></>;
+  return (
+    <>
+      <header className="brasta-site-nav">
+        <div className="brasta-site-nav-inner">
+          <a className="brasta-site-brand" href="/" aria-label="Brasta home">Brasta</a>
+          <div className="brasta-site-nav-account-space" aria-hidden="true" />
+        </div>
+      </header>
+      <BrastaBootstrap />
+      <AccountBridge />
+    </>
+  );
 }
